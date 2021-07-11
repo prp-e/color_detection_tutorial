44c8523f09a88bf40ac671cb5c5dffcc7087f368
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-07-12 00:31:26 +0430

    Skeleton for a video tutorial

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np 
 
 image = cv2.imread('tesla-model-s.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+# [160, 100, 100], [179, 255, 255]
 
 cv2.imshow('Tesla', image)
 if cv2.waitKey(0) & 0xff == ord('q'):
